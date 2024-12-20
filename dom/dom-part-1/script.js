7ebe126e874475989121847b04919f781a1ab198
Aashish Kumar Ray <2002raysi@gmail.com>
2024-12-20 20:21:59 +0530

    DOM Manipulation

---
+++ dom/dom-part-1/script.js
@@ -0,0 +1,35 @@
+
+///// Why you're not working brother ??
+
+
+const dom = document.querySelector("#container");
+// dom.innerHTML = "<h1> Initial Text </h1>";
+
+// dom.addEventListener("click", () => dom.innerHTML = "<h1>After click text</h1>");
+// ^
+// |
+// |
+// | worked fine
+
+// -------------------------------------
+
+
+// dom.innerHTML = "innerHTML hai be ye"
+
+// dom.innerText = "Hello Guys";
+
+// dom.textContent = "kya re kya dekh raha hai ?";
+
+// console.log(dom.innerHTML);
+// console.log(dom.innerText);
+// console.log(dom.textContent);
+
+
+//Styling -----> All the styling that can be done in css can also be done in js using .style.property 
+
+//Example
+
+
+// dom.style.backgroundColor = "white";
+// dom.style.backgroundColor = "black";
+
