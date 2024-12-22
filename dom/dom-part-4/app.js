4cf5adea07c65cdfc1cc4e41ad0f7597b06d43d0
Aashish Kumar Ray <2002raysi@gmail.com>
2024-12-22 22:01:43 +0530

    DOM event Handeling

--- dom/dom-part-4/app.js
+++ dom/dom-part-4/app.js
@@ -6,9 +6,23 @@
 //     const y = e.clientY;
 
 
-//     circle.createElement("")
 //     circle.style.left = `${e.clientX - 10}px`; // Center horizontally
 //     circle.style.top = `${e.clientY - 10}px`; 
 
 
 // })
+
+const newCircle = document.createElement("circleEverywhere");
+newCircle.addEventListener("mousemove", (e) => {
+    newCircle.innerHTML = `<div class="circleEverywhere">I am new Circle</div>
+    <style> .circleEverywhere{
+    position: absolute;
+    height: 10px;
+    width: 10px;
+    background-color: #f18238;
+    border-radius: 50%;
+} </style>
+    `;
+    newCircle.style.top = `${e.clientY - 10}px`;
+    newCircle.style.left = `${e.clientX - 10}px`;
+});
