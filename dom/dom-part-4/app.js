0040e3218bcf81b83f2215778fac037a566a73f0
Aashish Kumar Ray <2002raysi@gmail.com>
2024-12-22 20:41:31 +0530

    DOM event Handeling

--- dom/dom-part-4/app.js
+++ dom/dom-part-4/app.js
@@ -1,14 +1,14 @@
 // DOM events
 
-const circle = document.querySelector(".circle");
-document.addEventListener("mousemove", (e) => {
-    const x = e.clientX;
-    const y = e.clientY;
+// const circle = document.querySelector(".circle");
+// document.addEventListener("mousemove", (e) => {
+//     const x = e.clientX;
+//     const y = e.clientY;
 
 
-    circle.createElement("")
-    circle.style.left = `${e.clientX - 10}px`; // Center horizontally
-    circle.style.top = `${e.clientY - 10}px`; 
+//     circle.createElement("")
+//     circle.style.left = `${e.clientX - 10}px`; // Center horizontally
+//     circle.style.top = `${e.clientY - 10}px`; 
 
 
-})
+// })
