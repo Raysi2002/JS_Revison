e2ff2e098280e0db7dc63821ac94f9629ca64c9a
Aashish Kumar Ray <2002raysi@gmail.com>
2024-12-23 20:09:48 +0530

    Advance DOM properties

--- dom/dom-part-4/app.js
+++ dom/dom-part-4/app.js
@@ -12,17 +12,32 @@
 
 // })
 
-const newCircle = document.createElement("circleEverywhere");
-newCircle.addEventListener("mousemove", (e) => {
-    newCircle.innerHTML = `<div class="circleEverywhere">I am new Circle</div>
-    <style> .circleEverywhere{
-    position: absolute;
-    height: 10px;
-    width: 10px;
-    background-color: #f18238;
-    border-radius: 50%;
-} </style>
-    `;
-    newCircle.style.top = `${e.clientY - 10}px`;
-    newCircle.style.left = `${e.clientX - 10}px`;
-});
+// const newCircle = document.createElement("circleEverywhere");
+// newCircle.addEventListener("mousemove", (e) => {
+//     newCircle.innerHTML = `<div class="circleEverywhere">I am new Circle</div>
+//     <style> .circleEverywhere{
+//     position: absolute;
+//     height: 10px;
+//     width: 10px;
+//     background-color: #f18238;
+//     border-radius: 50%;
+// } </style>
+//     `;
+//     newCircle.style.top = `${e.clientY - 10}px`;
+//     newCircle.style.left = `${e.clientX - 10}px`;
+// });
+
+
+//More Properties
+const more = document.querySelector(".more");
+more.addEventListener("click", (e) => {
+    console.log("Time Stamp: " , e.timeStamp);
+    console.log(`Which : ${e.which}`);
+    console.log(`Bubble: ${e.bubbles}`);
+    console.log(`Cancelabe: ${e.cancelable}`);
+    console.log(`Key: ${e.key}`);
+    console.log(`PageX: ${e.pageX} PageY: ${e.pageY}`);
+    console.log(`Current Target: ${e.currentTarget}`);
+    console.log(`Type: ${e.type}`);
+    console.log(`Target: ${e.target}`)
+})
